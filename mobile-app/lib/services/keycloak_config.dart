9facb1f6b6c0570370db5dc19bdd483db2e27ce6
mrYumeth <yumethindiv123@gmail.com>
2026-07-25 16:15:37 +0530

    Mobile App Update - Complete fix for Keycloak login

--- mobile-app/lib/services/keycloak_config.dart
+++ mobile-app/lib/services/keycloak_config.dart
@@ -1,5 +1,5 @@
 class KeycloakConfig {
-  static const String issuer = 'http://10.0.2.2:8080/realms/fieldsync';
+  static const String issuer = 'http://192.168.43.222:8080/realms/fieldsync';
   static const String clientId = 'fieldsync-mobile';
   static const String redirectUrl = 'com.example.mobileapp:/oauthredirect';
   static const String discoveryUrl = '$issuer/.well-known/openid-configuration';
