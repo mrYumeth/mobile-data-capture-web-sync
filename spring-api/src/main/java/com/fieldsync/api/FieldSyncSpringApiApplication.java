08eb246b105142b380856607466f48018086f44c
mrYumeth <yumethindiv123@gmail.com>
2026-08-27 01:39:42 +0530

    Add FieldSync Spring Boot API foundation

---
+++ spring-api/src/main/java/com/fieldsync/api/FieldSyncSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.fieldsync.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldSyncSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldSyncSpringApiApplication.class, args);
+	}
+
+}
---
+++ spring-api/src/test/java/com/fieldsync/api/FieldSyncSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fieldsync.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldSyncSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
